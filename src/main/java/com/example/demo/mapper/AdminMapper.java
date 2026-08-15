d65024319e5cfcaef6e1e1d8228b4a59f121d6aa
valisoa01 <valisoatolotriniaina@gmail.com>
2026-08-15 23:18:21 +0300

    fix(format):mapper/

--- src/main/java/com/example/demo/mapper/AdminMapper.java
+++ src/main/java/com/example/demo/mapper/AdminMapper.java
@@ -1,4 +1,3 @@
 package com.example.demo.mapper;
 
-public class AdminMapper {
-}
+public class AdminMapper {}
--- src/main/java/com/example/demo/mapper/StudentMapper.java
+++ src/main/java/com/example/demo/mapper/StudentMapper.java
@@ -1,4 +1,3 @@
 package com.example.demo.mapper;
 
-public class StudentMapper {
-}
+public class StudentMapper {}
