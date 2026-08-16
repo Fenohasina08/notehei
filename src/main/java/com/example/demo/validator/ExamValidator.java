b62d765406d662ad2b9f2ef766f7bda9e80a5d87
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-08-16 15:44:39 +0300

    chore(validator): Transcript validator - validate the request if the requester is admin or the person who has the id student allowed to do the act

--- src/main/java/com/example/demo/validator/ExamValidator.java
+++ src/main/java/com/example/demo/validator/ExamValidator.java
@@ -14,33 +14,33 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class ExamValidator {
 
-    private static final BigDecimal FULL_WEIGHTING = new BigDecimal("100.00");
+  private static final BigDecimal FULL_WEIGHTING = new BigDecimal("100.00");
 
-    private final ExamRepository examRepository;
+  private final ExamRepository examRepository;
 
-    public void validateWeighting(JExam exam) {
-        if (exam.getType() == RETAKE) {
-            return;
-        }
-        var futureTotal = nonRetakeWeightingTotal(exam.getCourseId()).add(exam.getWeighting());
-        if (futureTotal.compareTo(FULL_WEIGHTING) > 0) {
-            throw new ExamValidationException(
-                    "The total weighting of course "
-                            + exam.getCourseId()
-                            + " would exceed 100% ("
-                            + futureTotal
-                            + ")");
-        }
+  public void validateWeighting(JExam exam) {
+    if (exam.getType() == RETAKE) {
+      return;
     }
-
-    public boolean isCourseWeightingComplete(UUID courseId) {
-        return nonRetakeWeightingTotal(courseId).compareTo(FULL_WEIGHTING) == 0;
+    var futureTotal = nonRetakeWeightingTotal(exam.getCourseId()).add(exam.getWeighting());
+    if (futureTotal.compareTo(FULL_WEIGHTING) > 0) {
+      throw new ExamValidationException(
+          "The total weighting of course "
+              + exam.getCourseId()
+              + " would exceed 100% ("
+              + futureTotal
+              + ")");
     }
+  }
 
-    private BigDecimal nonRetakeWeightingTotal(UUID courseId) {
-        return examRepository.findByCourseId(courseId).stream()
-                .filter(exam -> exam.getType() != RETAKE)
-                .map(JExam::getWeighting)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
+  public boolean isCourseWeightingComplete(UUID courseId) {
+    return nonRetakeWeightingTotal(courseId).compareTo(FULL_WEIGHTING) == 0;
+  }
+
+  private BigDecimal nonRetakeWeightingTotal(UUID courseId) {
+    return examRepository.findByCourseId(courseId).stream()
+        .filter(exam -> exam.getType() != RETAKE)
+        .map(JExam::getWeighting)
+        .reduce(BigDecimal.ZERO, BigDecimal::add);
+  }
 }
