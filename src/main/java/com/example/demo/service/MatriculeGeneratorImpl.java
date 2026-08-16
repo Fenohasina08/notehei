67d8b3724cc50ecc685776eca1e5d5020ed0847c
valisoa01 <valisoatolotriniaina@gmail.com>
2026-08-16 14:04:02 +0300

    fix(MatriculeGeneratoImp): %02 to %03

--- src/main/java/com/example/demo/service/MatriculeGeneratorImpl.java
+++ src/main/java/com/example/demo/service/MatriculeGeneratorImpl.java
@@ -27,6 +27,6 @@ public class MatriculeGeneratorImpl implements MatriculeGenerator {
 
     int year = LocalDate.now().getYear() % 100;
 
-    return String.format("%s%02d%d", prefix, year, sequenceValue);
+    return String.format("%s%03d%d", prefix, year, sequenceValue);
   }
 }
