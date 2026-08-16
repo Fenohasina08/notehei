829bd406afdcd0d457baa36d9a7b6c16532d03f8
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-08-16 10:59:07 +0300

    chore(Exam): Exam file will be the intermediate between ExamController and the API's clients

--- src/main/java/com/example/demo/model/Exam.java
+++ src/main/java/com/example/demo/model/Exam.java
@@ -5,4 +5,4 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record Exam(
-        UUID id, UUID courseId, ExamType type, LocalDateTime examDate, BigDecimal weighting) {}
+    UUID id, UUID courseId, ExamType type, LocalDateTime examDate, BigDecimal weighting) {}
