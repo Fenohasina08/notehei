f6539c05309f4b9e38b4723cedec950cbe541ba5
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-08-17 14:17:09 +0300

    chore(service): update courseservice with course validator

--- src/main/java/com/example/demo/service/CourseService.java
+++ src/main/java/com/example/demo/service/CourseService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.mapper.CourseMapper;
 import com.example.demo.model.Course;
 import com.example.demo.repository.CourseRepository;
+import com.example.demo.validator.CourseValidator;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -13,10 +14,12 @@ import org.springframework.stereotype.Service;
 public class CourseService {
 
   private final CourseRepository courseRepository;
+  private final CourseValidator courseValidator;
   private final CourseMapper courseMapper;
 
   public Course createCourse(Course course) {
     var entity = courseMapper.toEntity(course);
+    courseValidator.validate(entity);
     return courseMapper.toDto(courseRepository.save(entity));
   }
 
