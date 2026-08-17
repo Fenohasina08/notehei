05edb25ee5cd732e61861777a1da8997a198d11e
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-08-18 01:04:17 +0300

    chore(test):update postgresconf

---
+++ src/main/java/com/example/demo/endpoint/controller/HelloWorldController.java
@@ -0,0 +1,4 @@
+package com.example.demo.endpoint.controller;
+
+public class HelloWorldController {
+}
---
+++ src/main/java/com/example/demo/endpoint/event/SendEmailRequestedService.java
@@ -0,0 +1,4 @@
+package com.example.demo.endpoint.event;
+
+public class SendEmailRequestedService {
+}
