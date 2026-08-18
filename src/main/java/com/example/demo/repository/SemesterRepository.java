ff62792e8892712fbd59a39651d7e30463d13602
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-08-18 13:53:12 +0300

    chore(repository): create SemesterRepo

--- src/main/java/com/example/demo/repository/SemesterRepository.java
+++ src/main/java/com/example/demo/repository/SemesterRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.repository;
 
-public class f {
-}
+import com.example.demo.entity.JSemester;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SemesterRepository extends JpaRepository<JSemester, UUID> {}
